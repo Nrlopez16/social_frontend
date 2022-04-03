ae5ea8a37edf19755f4b11f3057af7c64fb37012
nrlopez <nrlopez16@hotmail.com>
2022-04-02 20:18:01 -0700

    each comment section able to open separate from all of them

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Body.css';
 import {getPosts} from '/home/nrlopez/social/src/api/index.js';
+import Post from '../Post/Post'
 
 
 function MainBody(){
     const [posted, setPosted] = useState([]);
-    const [showComments, setShowComments] = useState(false);
 
     useEffect(() => {
         getPosts()
@@ -22,30 +22,8 @@ function MainBody(){
     return (
         <div className="homepage">
             {posted.map(({id, username, profilePic, text, comments}) => (
-                <div key={ id } className="post"> 
-                <div className="postbody">
-                    <div className="main-body">
-                <img className="profilePicture" src={profilePic} />
-                <div className="content">
-                    <h2 className="username">{username}</h2>
-                    <h3 className="text">{text}</h3>
-                    </div>
-                    </div>
-                <button className="comment-toggle"
-                onClick={() => {setShowComments(!showComments)
-                    console.log(showComments)}}> Comments </button>
-                </div>
-                 
-                {comments.map(({id, profilePic, text, username}) => (
-                    <div key={ id } className={showComments ? "comment": "comment-invis" }> 
-                    <img className="propic" src={profilePic}/>
-                    <div className="content">
-                        <h2 className="username">{username}</h2>
-                        <h3 className="text">{text}</h3>
-                        </div>
-                    </div>
-                ))} 
-                </div>
+                <Post key={id} username={username} profilePic={profilePic}
+                text={text} comments={comments} />
             ))}
         </div>
     )
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,38 @@
+import React, {useState} from 'react';
+import './Post.css';
 
 
 //posts should be pushed into here to create a post per each one found from api call,
 //then each post will have its own id essentially to open their own comments section
 //one at a time and not all together
+
+function Post(props){
+    const {key, profilePic, username, text, comments} = props;
+    const [showComments, setShowComments] = useState(false);
+    return <div key={ key } className="post"> 
+    <div className="postbody">
+        <div className="main-body">
+    <img className="profilePicture" src={profilePic} />
+    <div className="content">
+        <h2 className="username">{username}</h2>
+        <h3 className="text">{text}</h3>
+        </div>
+        </div>
+    <button className="comment-toggle"
+    onClick={() => {setShowComments(!showComments)
+        console.log(showComments)}}> Comments </button>
+    </div>
+     
+    {comments.map(({id, profilePic, text, username}) => (
+        <div key={ id } className={showComments ? "comment": "comment-invis" }> 
+        <img className="propic" src={profilePic}/>
+        <div className="content">
+            <h2 className="username">{username}</h2>
+            <h3 className="text">{text}</h3>
+            </div>
+        </div>
+    ))} 
+    </div>
+}
+
+export default Post;
